9a83ec9e38388918221653049dd68ff321c0af09
Wilmer856 <joelguzman2468@gmail.com>
2023-12-01 14:08:14 -0500

    added CORS for testing

--- predatorDetection/settings.py
+++ predatorDetection/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = ['167.172.236.208']
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    'http://127.0.0.1:5500/',
+    "http://localhost:5500",
     'http://localhost'
 ]
 
